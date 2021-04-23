a84fdcf3e7eeb72980cae8af3267c5be01e84673
thomalexg <thomas.gae@posteo.de>
2021-04-23 12:20:30 +0200

    Make picture responsive

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Meme(props) {
   return (
     <div className="Meme">
-      <img alt="create inside joke" src={props.url} />
+      <img className='img' alt="create inside joke" src={props.url} />
     </div>
   );
 }
